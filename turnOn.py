e710797b41a65109e6eae05f38089748088d2d89
thiago514 <thiagogogola9@gmail.com>
2023-07-19 18:35:51 -0300

    log

--- turnOn.py
+++ turnOn.py
@@ -2,7 +2,7 @@ import subprocess, datetime, platform
 
 # https://pt.stackoverflow.com/questions/402231/como-executar-programa-em-python-ao-iniciar-linux-mint#:~:text=ExecStart%3D%2Fusr%2Fbin%2Fpython%20%2Fhome%2Fmeuscript.py%20%3E%20%2Fhome%2Fmeuscript.log%202%3E%261%20OBS%3A%20pode%20salvar,sudo%20systemctl%20daemon-reload%20%26%26%20sudo%20systemctl%20enable%20meuscript.service
 
-subprocess.call(args=["git push"], shell=True)
+subprocess.call(args=["git push "], shell=True)
 
 process = subprocess.call(args=["git status"], shell=True)
 
@@ -10,7 +10,7 @@ print(platform.node())
 
 if str(platform.node()).__contains__("not"):
 
-    inputLog = "\n Notebook Ligado: " + str(datetime.datetime.today().strftime('%Y/%m/%d %H:%M:%S'))
+    inputLog = "\n   Notebook Ligado: " + str(datetime.datetime.today().strftime('%Y/%m/%d %H:%M:%S'))
 
 if str(platform.node()).__contains__("pc"):
 
@@ -24,7 +24,7 @@ subprocess.call(args=["git add ."], shell=True)
 
 subprocess.call(args=["git commit -m log"], shell=True)
 
-subprocess.call(args=["git push"], shell=True)
+subprocess.call(args=["git push origin main"], shell=True)
 
 # print("data: ", datetime.datetime.now())
 
