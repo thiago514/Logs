7124cb779bb06d48d706b07ce50ba1542d1658d2
thiago514 <thiagogogola9@gmail.com>
2023-07-19 18:45:53 -0300

    log

--- turnOn.py
+++ turnOn.py
@@ -2,7 +2,7 @@ import subprocess, datetime, platform
 
 # https://pt.stackoverflow.com/questions/402231/como-executar-programa-em-python-ao-iniciar-linux-mint#:~:text=ExecStart%3D%2Fusr%2Fbin%2Fpython%20%2Fhome%2Fmeuscript.py%20%3E%20%2Fhome%2Fmeuscript.log%202%3E%261%20OBS%3A%20pode%20salvar,sudo%20systemctl%20daemon-reload%20%26%26%20sudo%20systemctl%20enable%20meuscript.service
 
-subprocess.call(args=["git pull --rebase"], shell=True)
+subprocess.call(args=["git pull --rebase origin main"], shell=True)
 
 process = subprocess.call(args=["git status"], shell=True)
 
